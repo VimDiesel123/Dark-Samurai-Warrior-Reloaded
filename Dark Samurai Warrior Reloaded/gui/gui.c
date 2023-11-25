c5fa3cfafa685f68c50e256f4188415e5b6da121
David Smith <dsmith141996@gmail.com>
2023-11-25 01:05:39 -0500

    Const everything

--- Dark Samurai Warrior Reloaded/gui/gui.c
+++ Dark Samurai Warrior Reloaded/gui/gui.c
@@ -2,8 +2,8 @@
 
 #include <string.h>
 
-static bool inside(V2 mousePos, V2 widgetPos, int widgetWidth,
-                   int widgetHeight) {
+static bool inside(const V2 mousePos, const V2 widgetPos, const int widgetWidth,
+                   const int widgetHeight) {
   return mousePos.x > widgetPos.x && mousePos.x < widgetPos.x + widgetWidth &&
          mousePos.y > widgetPos.y && mousePos.y < widgetPos.y + widgetHeight;
 }
@@ -12,20 +12,23 @@ static int center(const int totalSpace, const int needed) {
   return (totalSpace - needed) / 2;
 }
 
-static V2 textPositionCenter(Font font, const char* text, V2 widgetPosition,
-                       int widgetWidth, int widgetHeight) {
-  int xPos = MAX(0, widgetPosition.x + center(widgetWidth, textWidth(font, text)));
-  int yPos = MAX(0, widgetPosition.y + center(widgetHeight, textHeight(font, text)));
+static V2 textPositionCenter(const Font font, const char* text,
+                             const V2 widgetPosition, const int widgetWidth,
+                             const int widgetHeight) {
+  int xPos =
+      MAX(0, widgetPosition.x + center(widgetWidth, textWidth(font, text)));
+  int yPos =
+      MAX(0, widgetPosition.y + center(widgetHeight, textHeight(font, text)));
   return (V2){xPos, yPos};
 }
 
-static int textWidth(Font font, const char* text) {
+static int textWidth(const Font font, const char* text) {
   size_t length = strlen(text);
   // TODO: (David) This only works because the font is monospaced;
   return length * font.advance_width;
 }
 
-static int textHeight(Font font, const char* text) {
+static int textHeight(const Font font, const char* text) {
   int currentTallestCharacter = 0;
   size_t length = strlen(text);
   for (size_t i = 0; i < length; ++i) {
@@ -36,15 +39,16 @@ static int textHeight(Font font, const char* text) {
   return currentTallestCharacter;
 }
 
-bool clickFinished(const UI context, u64 id) {
+bool clickFinished(const UI context, const u64 id) {
   return context.active == id && context.mouseButtonUp && context.focused == id;
 }
 
-bool clickStarted(const UI context, u64 id) {
+bool clickStarted(const UI context, const u64 id) {
   return context.focused == id && context.mouseButtonDown;
 }
-bool button(UI* context, u64 id, LoadedBitmap* bitmap, V2 pos, int width,
-            int height, Color color, Font* font, const char* text) {
+bool button(UI* context, const u64 id, LoadedBitmap* bitmap, const V2 pos,
+            const int width, const int height, const Color color,
+            const Font* font, const char* text) {
   draw_rectangle(bitmap, pos.x, pos.y, width, height, color);
   V2 textPos = textPositionCenter(*font, text, pos, width, height);
   draw_string(bitmap, font, textPos.x, textPos.y, text);
